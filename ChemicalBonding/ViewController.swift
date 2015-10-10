deb659d62a3e6b102eaaba5bb11b5f0de34ee33e
Sarah Hinsley <sarah.hinsley@virginmedia.com>
2015-10-10 16:45:47 +0100

    Initial Commit

---
+++ ChemicalBonding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChemicalBonding
+//
+//  Created by Sarah Hinsley on 10/10/2015.
+//  Copyright © 2015 Sarah Hinsley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
